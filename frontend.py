a0f7062a9146decf3ed6ed8289e360b13c0fed31
odaesa <devnull@localhost>
2011-07-07 09:13:56 +0100

    adding functionality to the frontend

--- frontend.py
+++ frontend.py
@@ -9,43 +9,98 @@ from serialisers.JsonSerialiser import JsonSerialiser
 render = web.template.render('templates/')
 
 urls = (
-    '/','index'
+    '/','index',
+    '/bibsoup','bibsoup',
+    '/bibsoup/record','record',
+    '/bibsoup/collection','collection',
+    '/bibsoup/person','person',
+    '/bibsoup/group','group'
 )
 
 app = web.application(urls,globals())
 
+# show splash page
+# should include link to create new
+# link to view collection
+# stats of collections available
 class index:
+    def upload(self,url,filename,sourcetype,collname):
+        # if url, get raw from url
+        if True:
+            location = urllib2.unquote(queries.get('loc'))
+        
+        # if file, get raw from file
+        
+        # if we already have this loc/file/coll indexed
+            # if needs updated
+                # update it
+
+        # if loc/file/coll not indexed
+        
+            # get the datasource
+            # will depend on sourcetype
+            ds = DataSource()
+            d = ds.import_from(location)
+        
+            # pass to solr
+            i = Indexer()
+            i.index(d)
+        
+            # pass to couch    
+            s = JsonSerialiser()
+            j = s.serialise(d)
+            r = dao()
+            r.relax(j)
+        
+        # go to page with index of the collection
+        return "done"
+
     def GET(self):
         # if passed a location
         if web.ctx.query:
             queries = dict(item.split("=") for item in str(web.ctx.query.replace("?","")).split("&"))
             if queries.get('loc'):
-                # test to see if we already have this loc indexed
-                # if so, test if it needs updated
-                # update if it needs it
-                # call the function that builds the output page
-                
-                # get the location
-                location = urllib2.unquote(queries.get('loc'))
-                
-                # get the datasource
-                ds = DataSource()
-                d = ds.import_from(location)
-                
-                # pass to solr
-                i = Indexer()
-                i.index(d)
-                
-                # pass to couch    
-                s = JsonSerialiser()
-                j = s.serialise(d)
-                r = dao()
-                r.relax(j)
-                
-                # fire it to the parser
-                return "ta! we will do something with " + location
+                # pass upload location, source type and name for collection to uploader
+                return upload(queries.get('loc'),False,"type","name")
 
-        # otherwise display a form to accept a URL
+        # otherwise display a default front page
         return render.index(False)
+
+    def POST(self):
+        # if passed a location from the form
+        if True:
+            return upload("location",False,"type","name")
+        
+        # if passed a file to upload
+        if False:
+            return upload(False,"filepointer","type","name")
+
+        # go to index of this collection
+        return "done"
+
+# the default faceted view across our whole dataset
+class bibsoup:
+    def GET(self):
+        return "hello"
+
+# content-negociate for a record
+class record:
+    def GET(self):
+        return "show a record"
+
+# content-negociate for a collection
+class collection:
+    def GET(self):
+        return "show a collection"
+
+# content-negociate for a person
+class person:
+    def GET(self):
+        return "show a person"
+
+# content-negociate for a group
+class group:
+    def GET(self):
+        return "show a person"
 
 if __name__ == "__main__": app.run()
