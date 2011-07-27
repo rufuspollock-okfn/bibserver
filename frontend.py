00fb6e673bfbb00b969572b4d6d2ee2d745ed714
markmacgillivray <devnull@localhost>
2011-07-27 18:08:31 +0100

    sending bibserver templates

--- frontend.py
+++ frontend.py
@@ -18,63 +18,59 @@ urls = (
 
 app = web.application(urls,globals())
 
-# show splash page  
 class index:
     def GET(self):
-        # if passed a query param
+        # if passed a source, upload it
         if web.input().get("source"):
-            # presuming its from bibtex at the moment
-            # get the data from the source
-            try:
-                source = urllib2.unquote(web.input().get("source"))
-                ds = DataSource()
-                data = ds.import_from(source)
-                # save the data
-                db = dao();
-                #db.save(data)
-                return "thanks! we are uploading from " + web.input().get("source")
-            except:
-                return "sorry. we could not upload from " + web.input().get("source")
+            upload( web.input().get("source") )
 
         # otherwise display a default front page
         return render.index(False)
-
+        
     def POST(self):
-        # if passed a source URL from the form
+        # if passed a source, upload it
         if web.input().get("source"):
-            # presuming its from bibtex at the moment
-            # get the data from the source
-            source = urllib2.unquote(queries.get('source'))
+            upload( web.input().get("source") )
+
+        # default to hitting the search result page
+        if web.input().get("q") == "":
+            raise web.seeother('/search')
+        else:
+            raise web.seeother('/search?q=' + web.input().get("q"))
+
+    def upload(source):
+        # presuming its from bibtex at the moment
+        # get the data from the source
+        try:
+            source = urllib2.unquote(web.input().get("source"))
             ds = DataSource()
             data = ds.import_from(source)
             # save the data
-            db = dao()
+            db = dao();
             #db.save(data)
             return "thanks! we are uploading from " + web.input().get("source")
+        except:
+            return "sorry. we could not upload from " + web.input().get("source")
 
-
-        # default to hitting the search result page
-        raise web.seeother('/search?q=' + web.input().get("q"))
-        
-# content-negociate for a record
 class record:
+    # do conneg here - should pass solreyes if html, should pass JSON object if JSON
     def GET(self,record):
-        return "show a record " + record
+        s = SolrEyesController()
+        settings = {"q":{"*":[record]},"base_url":"http://bibserver.cottagelabs.com/record/" + record}
+        return s.GET(settings)
 
-# pass collections info to the requester
 class collection:
-
     # do conneg here - should pass solreyes if html, should pass JSON object if JSON
     def GET(self,collection):
         s = SolrEyesController()
         settings = {"q":{"collection":[collection]},"base_url":"http://bibserver.cottagelabs.com/collection/" + collection}
         return s.GET(settings)
 
-# content-negociate for a person
 class person:
+    # do conneg here - should pass solreyes if html, should pass JSON object if JSON
     def GET(self,person):
-        # if a string, do a search for the string
-        # if a person ID, get the person record
-        return "show a person " + person
+        s = SolrEyesController()
+        settings = {"q":{"*":[person]},"base_url":"http://bibserver.cottagelabs.com/person/" + person}
+        return s.GET(settings)
 
 if __name__ == "__main__": app.run()
