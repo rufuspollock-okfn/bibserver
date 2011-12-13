13e1165f9068c087a96e83cca3f06460a192f801
rgrp <rufus.pollock@okfn.org>
2011-12-13 04:51:41 +0000

    [deploy,bugfix][s]: correct config.py to look for config.json relative to config.py rather than just for 'config.json' since latter does not work in deployed environments (e.g. with modwsgi).

--- bibserver/config.py
+++ bibserver/config.py
@@ -1,3 +1,4 @@
+import os
 import json
 
 '''read the config.json file and make available as a config dict'''
@@ -18,7 +19,10 @@ def load_config(path):
 
     return out
 
-config = load_config('config.json')
+here = os.path.dirname(__file__)
+parent = os.path.dirname(here)
+config_path = os.path.join(parent, 'config.json')
+config = load_config(config_path)
 
 __all__ = ['config']
 
