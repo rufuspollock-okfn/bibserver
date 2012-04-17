c1ce6b2f6ec2b33189c8db8acfab093dc8d7e39b
Etienne Posthumus <eposthumus@gmail.com>
2012-04-17 14:11:58 +0200

    Improve author handling, splitting on commas Add a -cache option to test locally without needing network access.

--- parserscrapers_plugins/wikipedia.py
+++ parserscrapers_plugins/wikipedia.py
@@ -25,20 +25,38 @@ def wikitext_to_dict(txt):
             if len(ccc) == 2:
                 tmp[ccc[0].strip()] = ccc[1].strip()
         if tmp:
+            if 'author' in tmp:
+                auth_string = tmp['author'].split(',')
+                tmp['author'] = []
+                for au in auth_string:
+                    au = au.strip()
+                    if au.startswith('and '):
+                        au = au[4:]
+                    tmp.setdefault('author', []).append({'name':au})
             name = '%s %s' % (tmp.get('first',''), tmp.get('last', ''))
             if name.strip():
-                tmp['author'] = [{'name':name}]
+                tmp.setdefault('author', []).append({'name':name})
             if 'journal' in tmp:
                 tmp['journal'] = {'name':tmp['journal']}
             buf.append(tmp)
     return buf
     
-def parse():
+def parse(local_cache):
     q = sys.stdin.read()
     URL = 'http://en.wikipedia.org/w/api.php?action=query&list=search&srlimit=50&srprop=wordcount&format=json&srsearch='
     URLraw = 'http://en.wikipedia.org/w/index.php?action=raw&title='
-    data = urllib2.urlopen(URL+urllib.quote_plus(q)).read()
-    data_json = json.loads(data)
+    if local_cache:
+        try:
+            cached_data = json.loads(open('wikipedia.py.data').read())
+            data_json = cached_data.get('data1', {})
+        except IOError:
+            cached_data = {'data1':{}, 'data2':{}}
+            data_json = False
+    if not data_json:
+        data = urllib2.urlopen(URL+urllib.quote_plus(q)).read()
+        data_json = json.loads(data)
+    if local_cache:
+        cached_data['data1'] = data_json
     records = []
     
     try:
@@ -48,7 +66,13 @@ def parse():
             if x['wordcount'] > 20:
                 quoted_title = urllib.quote_plus(x['title'].encode('utf8'))
                 try:
-                    title_data = urllib2.urlopen(URLraw+quoted_title).read()
+                    title_data = None
+                    if local_cache:
+                        title_data = cached_data.get('data2',{}).get(quoted_title)
+                    if title_data is None:
+                        title_data = urllib2.urlopen(URLraw+quoted_title).read()
+                    if local_cache:
+                        cached_data.setdefault('data2', {})[quoted_title] = title_data
                 except httplib.BadStatusLine:
                     sys.stderr.write('Problem reading %s\n' % (URLraw+quoted_title))
                     continue
@@ -58,8 +82,10 @@ def parse():
                         c['link'] = [{'url':'http://en.wikipedia.org/wiki/'+quoted_title}]
                     records.extend(citations)
     except:
-        sys.stderr.write(data+'\n\n'+traceback.format_exc())
+        sys.stderr.write(traceback.format_exc())
     sys.stdout.write(json.dumps({'records':records, 'metadata':{}}))
+    if local_cache:
+        open('wikipedia.py.data', 'w').write(json.dumps(cached_data))
     
 def main():
     conf = {"display_name": "Wikipedia search to citations",
@@ -67,12 +93,15 @@ def main():
             "downloads": True,
             "contact": "openbiblio-dev@lists.okfn.org", 
             "bibserver_plugin": True, 
-            "BibJSON_version": "0.81"}        
+            "BibJSON_version": "0.81"}
+    local_cache = False        
     for x in sys.argv[1:]:
         if x == '-bibserver':
             sys.stdout.write(json.dumps(conf))
             sys.exit()
-    parse()
+        elif x == '-cache':
+            local_cache = True
+    parse(local_cache)
             
 if __name__ == '__main__':
     main()
